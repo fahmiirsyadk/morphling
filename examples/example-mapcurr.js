afd23f4650c847c1a0e381b27c8ddddedc81aec2
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2019-05-23 01:12:09 +0700

    add experimental mapcurr

---
+++ examples/example-mapcurr.js
@@ -0,0 +1,25 @@
+const { pipe, join } = require("../index");
+const { tag } = require("../dom");
+
+const map = expression => arr => {
+  console.log(typeof expression);
+  return typeof expression == "function"
+    ? arr.map(res => expression(res))
+    : typeof expression == "string"
+    ? arr.map(res => res[expression])
+    : arr.map(res => res);
+};
+
+const listGroupItem = tag({ tag: "li", attr: { class: "list-group-item" } });
+const shoplists = [
+  { id: 1, name: "banana", type: "fruit" },
+  { id: 2, name: "skate", type: "sport-tool" },
+  { id: 3, name: "knife", type: "kitchen-tool" },
+  { id: 3, name: "knife", type: "kitchen-tool" }
+];
+
+const mapdep = items => pipe(join(""))(map(listGroupItem)(items));
+// const mapdep = items => items.map(listGroupItem).join("");
+
+listGroupItem("Cras justo");
+console.log(mapdep(["Cras justo", "Dapibus ac"]));
